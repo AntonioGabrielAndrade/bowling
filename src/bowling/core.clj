d931b7bfafe633a259b877f9143db6c9b747359b
Antonio Gabriel Pereira de Andrade <gabriel.ecomp@gmail.com>
2018-08-21 00:08:49 -0300

    Group frames for simple games

--- src/bowling/core.clj
+++ src/bowling/core.clj
@@ -1,5 +1,11 @@
 (ns bowling.core)
 
+(defn group-frames [rolls]
+  (partition 2 rolls))
+
+(defn frame-score [frame]
+  frame)
+
 (defn score [rolls]
-  (reduce + rolls))
+  (reduce + (map frame-score (group-frames rolls))))
 
--- test/bowling/core_test.clj
+++ test/bowling/core_test.clj
@@ -2,10 +2,17 @@
   (:require [clojure.test :refer :all]
             [bowling.core :refer :all]))
 
+(deftest group-frames-test
+  (testing "group frames for simple game"
+    (is (= '((1 0) (1 0) (1 0) (1 0) (1 0) (1 0) (1 0) (1 0) (1 0) (1 0))
+           (group-frames [1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0])))))
 
 (deftest score-test
   (testing "zero score game"
-    (is (= 0 (score (repeat 20 0)))))
+    '(is (= 0 (score (repeat 20 0)))))
 
   (testing "score for simple game"
-    (is (= 20 (score (repeat 20 1))))))
+    '(is (= 20 (score (repeat 20 1)))))
+
+  (testing "score for one spare game"
+    '(is (= 14 (score (concat [5 5 2] (repeat 17 0)))))))
